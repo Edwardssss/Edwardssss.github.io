c22fcd0189576efbf9870a998efb809f3b83cde2
Edwardssss <2021010901017@std.uestc.edu.cn>
2023-06-25 01:09:03 +0800

    Site updated: 2023-06-25 01:08:56

---
+++ js/giscus.js
@@ -0,0 +1,19 @@
+if(window.location.href != "https://edwardssss.github.io/")
+{
+    var GiscusScript = document.createElement("script");
+    GiscusScript.setAttribute("type", "text/javascript");
+    GiscusScript.setAttribute("src","https://giscus.app/client.js");
+    GiscusScript.setAttribute("data-repo","Edwardssss/Blog_giscus");
+    GiscusScript.setAttribute("data-repo-id","R_kgDOI249EA");
+    GiscusScript.setAttribute("data-category","Announcements");
+    GiscusScript.setAttribute("data-category-id","DIC_kwDOI249EM4CT2tV");
+    GiscusScript.setAttribute("data-mapping","title");
+    GiscusScript.setAttribute("data-strict","1");
+    GiscusScript.setAttribute("data-reactions-enabled","1");
+    GiscusScript.setAttribute("data-emit-metadata","1");
+    GiscusScript.setAttribute("data-input-position","top");
+    GiscusScript.setAttribute("data-theme","light_protanopia");
+    GiscusScript.setAttribute("data-lang","zh-CN");
+    GiscusScript.setAttribute("crossorigin","anonymous");
+    GiscusScript.setAttribute("async","async");
+}
